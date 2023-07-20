ebf714d4ea0a59595d02ce5146ed3bc99b3ea433
tpn168 <127319660+tpn168@users.noreply.github.com>
2023-07-20 11:07:38 +0700

    Update Operation Form.py
    make some data non-edit

--- Operation Form.py
+++ Operation Form.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 
+def submit_form():
+    print("Form submitted!")  # Modify this function to handle form submission.
+
 def create_page(notebook, page_name):
     page = ttk.Frame(notebook)
     notebook.add(page, text=page_name)
@@ -27,18 +30,16 @@ def create_page(notebook, page_name):
     status_dropdown = ttk.Combobox(frame1, values=status_options, width=60)
     status_dropdown.grid(row=len(labels1), column=1)
 
-    # ... the rest of the code is the same ...
-
-
     frame2 = ttk.LabelFrame(page, text="Noon Report")
     frame2.grid(row=1, column=0, padx=10, pady=10, sticky='w')
 
     labels2 = ['M/E RPM']
-
     sub_labels = [
         ("Fresh Water", ['Fresh Water Consumed', 'Fresh Water Received']),
-        ("MGO USED ปริมาณใช้น้ำมันMGO", ['A/E#1', 'A/E#2', 'A/E#3', 'Boiler', 'Cargo Pump', 'M/E', 'Diesel Received']),
-        ("MFO USED ปริมาณใช้น้ำมันเตา", ['Boiler', 'M/E', 'Total MFO Used', 'MFO Received']),
+        ("MGO USED ปริมาณใช้น้ำมันMGO", ['A/E#1', 'A/E#2', 'A/E#3', 'Boiler', 
+                                           'Cargo Pump' if page_name not in ['RM2', 'RM5', 'RM7'] else None, 
+                                           'M/E', 'Diesel Received']),
+        ("MFO USED ปริมาณใช้น้ำมันเตา", ['Boiler', 'M/E', 'MFO Received' if page_name != 'RM1' else None]),
         ("System Lube Oil", ['Used', 'Received']),
         ("A/E Lube. Oil", ['Used', 'Received']),
         ("Cylinder Oil เฉพาะลำ1,5,7", ['Used', 'Received']),
@@ -55,24 +56,25 @@ def create_page(notebook, page_name):
         ttk.Label(frame2, text=title, foreground="blue", font=('Arial', 10, 'bold')).grid(row=r, column=0, sticky='w')
         r += 1
         for label in labels:
-            ttk.Label(frame2, text=label).grid(row=r, column=0, sticky='w')
-            ttk.Entry(frame2).grid(row=r, column=1)
-            r += 1
+            if label is not None:
+                ttk.Label(frame2, text=label).grid(row=r, column=0, sticky='w')
+                if (title == "MFO USED ปริมาณใช้น้ำมันเตา" and page_name in ['RM1', 'RM2', 'SM3']) or \
+                   (title == "Cylinder Oil เฉพาะลำ1,5,7" and page_name in ['RM2', 'SM3']) or \
+                   (label == "A/E#3" and page_name in ['SM3', 'RM5']):
+                    ttk.Entry(frame2, state='disabled').grid(row=r, column=1)
+                else:
+                    ttk.Entry(frame2).grid(row=r, column=1)
+                r += 1
 
-    submit_button = ttk.Button(page, text="Submit", command=lambda: print("Form submitted!"))  # Modify this function to handle form submission.
+    submit_button = ttk.Button(page, text="Submit", command=submit_form)
     submit_button.grid(row=2, column=0, pady=10)
 
+root = tk.Tk()
+notebook = ttk.Notebook(root)
+page_names = ["RM1", "RM2", "RM5", "RM7", "SM3"]
 
-def main():
-    root = tk.Tk()
+for name in page_names:
+    create_page(notebook, name)
 
-    notebook = ttk.Notebook(root)
-    notebook.grid(row=0, column=0, padx=10, pady=10)
-
-    for page_name in ['RM1', 'RM2', 'RM5', 'RM7', 'SM3']:
-        create_page(notebook, page_name)
-
-    root.mainloop()
-
-if __name__ == "__main__":
-    main()
+notebook.pack(expand=1, fill="both")
+root.mainloop()
